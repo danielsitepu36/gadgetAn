94971011570b9a8df7625fdd1b2b75c65cfb0a67
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-29 19:01:14 +0700

    2 font

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./screens/home";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return <Home />;
+  const [fontsLoadded, setFontsLoaded] = useState(false);
+
+  if (fontsLoadded) {
+    return (<Home />);
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
 }
