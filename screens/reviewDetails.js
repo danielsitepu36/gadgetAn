157338dd537f5fd1c180509e4db6e7c097fed766
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-30 08:56:48 +0700

    10-Card

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function ReviewDetails({ route }) {
   return (
     <View style={globalStyles.container}>
-      <Text>{route.params?.title ?? "Title"}</Text>
-      <Text>{route.params?.body ?? "Body"}</Text>
-      <Text>{route.params?.rating ?? "Rating"}</Text>
+      <Card>
+        <Text>{route.params?.title ?? "Title"}</Text>
+        <Text>{route.params?.body ?? "Body"}</Text>
+        <Text>{route.params?.rating ?? "Rating"}</Text>
+      </Card>
     </View>
   );
 }
