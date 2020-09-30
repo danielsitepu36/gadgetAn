62d623993b570c4c178a60cad8d2c53fe1a23fdd
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-30 09:31:01 +0700

    11-Images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
 import Card from "../shared/card";
 
 export default function ReviewDetails({ route }) {
+  const rating = route.params?.rating ?? "1";
+
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{route.params?.title ?? "Title"}</Text>
         <Text>{route.params?.body ?? "Body"}</Text>
-        <Text>{route.params?.rating ?? "Rating"}</Text>
+        <View style={styles.rating}>
+          <Text>GadgetAn rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+});
